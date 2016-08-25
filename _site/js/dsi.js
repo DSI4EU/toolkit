fc75ad3123828f4988d6408a3dad2737b6d4743c
matlo <matteo.loglio@gmail.com>
2016-08-25 17:35:04 +0200

    index

---
+++ _site/js/dsi.js
@@ -0,0 +1,108 @@
+/* jshint -W033 */
+
+//testing
+function scrollToButtons(){
+  var top = parseInt($("#page-content").offset().top)
+  $("body").scrollTo(0, top);
+}
+
+//hamburger menu
+$('#toggle').click(function() {
+   $(this).toggleClass('active')
+   $('#overlay').toggleClass('open')
+   $('#light-logo').toggleClass('fade')
+   $('#dark-logo').toggleClass('fade')
+  })
+
+//Replace all SVG images with inline SVG
+jQuery('img.svg').each(function(){
+  var $img = jQuery(this)
+  var imgID = $img.attr('id')
+  var imgClass = $img.attr('class')
+  var imgURL = $img.attr('src')
+
+  jQuery.get(imgURL, function(data) {
+      // Get the SVG tag, ignore the rest
+      var $svg = jQuery(data).find('svg')
+
+      // Add replaced image's ID to the new SVG
+      if(typeof imgID !== 'undefined') {
+          $svg = $svg.attr('id', imgID)
+      }
+      // Add replaced image's classes to the new SVG
+      if(typeof imgClass !== 'undefined') {
+          $svg = $svg.attr('class', 'replaced-svg')
+      }
+
+      // Remove any invalid XML tags as per http://validator.w3.org
+      $svg = $svg.removeAttr('xmlns:a')
+
+      // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+      if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+          $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+      }
+
+      // Replace image with new SVG
+      $img.replaceWith($svg)
+
+  }, 'xml')
+})
+
+//autogrow input
+function auto_grow(element) {
+    element.style.height = "5px"
+    element.style.height = (element.scrollHeight)+"px"
+}
+
+/* QUESTIONS FOCUS */
+
+//keyboard focus
+$("textarea").focus(function() {
+  $("body").find(".active").removeClass("active")
+  $(this).parent().addClass("active")
+})
+
+//smooth scroll
+$(".question textarea").focus(function() {
+  $('html, body').animate({
+        scrollTop: $(this).parent().offset().top - 100
+    }, 400);
+})
+
+//keyboard focusout
+$("textarea").focusout(function() {
+  $(this).parent().removeClass("active")
+})
+
+//mouseclick
+$(".question").click(function(){
+  $(".active").removeClass("active")
+  $(this).addClass("active")
+  $(this).find("textarea").focus()
+})
+
+$(".indicator-input").hide()
+$(".define-link").click(function() {
+  $(this).parent().parent().find(".indicator-input").fadeIn()
+  $(this).hide()
+})
+
+$(".indicator-send").click(function(){
+  $(this).parent().parent().fadeOut()
+})
+
+//https://css-tricks.com/snippets/jquery/smooth-scrolling/
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 100
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
--- index.md
+++ index.md
@@ -1,6 +1,38 @@
 ---
 layout: index
 title: DSI Toolkito
+
+# HERO #
 hero: Digital social innovation <br> &#35;toolkit
 subtitle: The DSI Toolkit is a collaboratively developed toolkit that helps projects to scale in a sustainable way, generating a better impact.
+hero-button: Explore the toolkit
+
+# COLUMNS #
+# L #
+left-image: "../assets/scale.svg"
+left-title: The DSI scale
+left-p: Helps to assess the DSI growth considering sharing, openness and societal good.
+left-button: Build the DSI scale
+# R #
+right-image: "../assets/peer.svg"
+right-title: Peer knowledge
+right-p: Wanna help other digital social innovators to scale? Share your knowledge!
+right-button: Contribute
+
+# BIG PICTURE #
+picture-title: Workshops
+picture-subtitle: Workshops, events and meetups are organized all around Europe to built a DSI community-generated knowledge and promote social sustainable growth.
+
+# WORKSHOPS #
+left-ws: Latest workshop
+left-link-text: Size matters?
+left-link: http://google.com
+left-desc: 31 May 2016, iMal, Brussels.
+
+right-ws: Next workshop
+right-link-text: DSI Hunt
+right-link: http://google.com
+right-desc: 12 Oct 2016, Maker Faire Rome.
+
+
 ---
