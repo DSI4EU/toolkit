7acefb4253dadd5d505c62d7c466bd4f2c5a3160
matlo <write@matlo.me>
2016-10-07 13:53:15 +0100

    sweet delaunay

--- _site/js/dsi.js
+++ _site/js/dsi.js
@@ -148,44 +148,44 @@ $('.quote').on('click', '.indicator-send', function(){
 
 $('.quote').on('click', '.reverse-card', function(){
   var p = $(this).parent().parent()
-  p.css("background", "#282930")  
+  p.css("background", "#282930")
   var content = p.data("content")
   p.html(content)
   $(this).parent().find(".scale-spinner").hide()
 })
 
 // index top margin
-function gradientMargins() {
-  var gradientHeight = $("#gradient-hero").outerHeight()
+function triangleMargins() {
+  var triangleHeight = $("#triangle-hero").outerHeight()
   var headerHeight = $("header").outerHeight()
-  gradientHeight -= headerHeight
-  $('#high-margin').css("margin-top", gradientHeight + "px")
-  if (gradientHeight > (window.innerHeight * 0.9)) {
+  triangleHeight -= headerHeight
+  $('#high-margin').css("margin-top", triangleHeight + "px")
+  if (triangleHeight > (window.innerHeight * 0.9)) {
     // console.log("bb")
-    // $("#gradient-hero").addClass("slim-screen-hero")
+    // $("#triangle-hero").addClass("slim-screen-hero")
   }
 }
-if ($("#gradient-hero").outerHeight() > (window.innerHeight * 0.9)) {
-  $("#gradient-hero").addClass("slim-screen-hero")
+if ($("#triangle-hero").outerHeight() > (window.innerHeight * 0.9)) {
+  $("#triangle-hero").addClass("slim-screen-hero")
 }
-gradientMargins()
+triangleMargins()
 
-//interactive gradient
-$(document).mousemove(function(e){
-    var m = {
-      x : e.pageX,
-      y : e.pageY
-    }
-    //radians
-    var adj = ($("#gradient-hero").outerWidth() * 0.5) - m.x
-    var opp = ($("#gradient-hero").outerHeight() * 0.5) - m.y
-    var tan = opp / adj
-    var atan = Math.atan(tan)
-    //degs
-    atan *= (180 / Math.PI)
-    atan = parseInt(Math.abs(atan*0.2))
-    $("#gradient-hero").css("background-image", "linear-gradient(" + atan + "deg, #7421D6 0%, #26C6A3 100%)")
-})
+// //interactive gradient
+// $(document).mousemove(function(e){
+//     var m = {
+//       x : e.pageX,
+//       y : e.pageY
+//     }
+//     //radians
+//     var adj = ($("#gradient-hero").outerWidth() * 0.5) - m.x
+//     var opp = ($("#gradient-hero").outerHeight() * 0.5) - m.y
+//     var tan = opp / adj
+//     var atan = Math.atan(tan)
+//     //degs
+//     atan *= (180 / Math.PI)
+//     atan = parseInt(Math.abs(atan*0.2))
+//     $("#gradient-hero").css("background-image", "linear-gradient(" + atan + "deg, #7421D6 0%, #26C6A3 100%)")
+// })
 
 // https://css-tricks.com/snippets/jquery/smooth-scrolling/
 $(function() {
@@ -253,6 +253,89 @@ $("#knowledge-send").click(function(){
   }
 })
 
-function goToThanks() {
+/////////// DELAUNAY
 
-}
+  var BLUE, GRAY;
+  var tris;
+
+  function setup(){
+      var w = ~~($("#triangle-hero").width());
+      var h = ~~($("#triangle-hero").height());
+      var myCanvas = createCanvas( w, h );
+      myCanvas.parent('triangle-hero');
+
+      BLUE = color( '#282930' );
+      GRAY = color( '#2C2D34' );
+
+      initializeTriangulation();
+  }
+
+  function initializeTriangulation(){
+      tris = [];
+      var pts = [];
+
+      // push canvas rect points
+      pts.push( createVector( 0, 0 ) );
+      pts.push( createVector( width, 0 ) );
+      pts.push( createVector( width, height ) );
+      pts.push( createVector( 0, height ) );
+
+      // ~~ truncates a floating point number and keeps the integer part, like floor()
+      var n = 7 + ~~(Math.random() * 5);
+      for( var i = 0; i < n; i ++ ){
+          pts.push( createVector( ~~ random( width ), ~~ random( height ) ) );
+      }
+
+      // Now, let's use Delaunay.js
+      // Delaunay.triangulate expect a list of vertices (which should be a bunch of two-element arrays, representing 2D Euclidean points)
+      // and it will return you a giant array, arranged in triplets, representing triangles by indices into the passed array
+      // Array.map function let us create an Array of 2 elements arrays [ [x,y],[x,y],..] from our array of PVector [ PVector(x,y), PVector(x,y), ... ]
+      var triangulation = Delaunay.triangulate( pts.map( function( pt ){
+          return [ pt.x, pt.y ];
+      } ) );
+
+      // create Triangles object using indices returned by Delaunay.triangulate
+      for( var i = 0; i < triangulation.length; i += 3 ){
+          tris.push( new Triangle(
+              pts[ triangulation[ i ] ],
+              pts[ triangulation[ i + 1 ] ],
+              pts[ triangulation[ i + 2 ] ]
+          ) );
+      }
+  }
+
+  // class for keeping triangles from 3 PVectors
+  function Triangle( _a, _b, _c ){
+      // PVectors
+      this.a = _a;
+      this.b = _b;
+      this.c = _c;
+
+      // used for fill using lerpColor
+      this.r = random(0.8);
+
+      this.draw = function(){
+          noStroke();
+          fill( lerpColor( GRAY, BLUE, this.r ) );
+
+          triangle( this.a.x, this.a.y, this.b.x, this.b.y, this.c.x, this.c.y );
+
+          stroke( BLUE );
+          strokeJoin( BEVEL );
+          strokeWeight( 6 );
+          noFill();
+          triangle( this.a.x, this.a.y, this.b.x, this.b.y, this.c.x, this.c.y );
+      };
+
+  }
+
+  function draw(){
+      background( BLUE );
+      tris.forEach(function (t) {
+        return t.draw();
+      });
+  }
+
+  function mousePressed(){
+      initializeTriangulation();
+  }
